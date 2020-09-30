476b9f4dcd8573d6e3794c2457307560d5f28879
lauren <lauren@MacBook-Pro-de-user.local>
2020-09-30 11:38:04 +0200

    After build

---
+++ public/precache-manifest.1de097999ac5a0a061914dd970735513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2930ac7583e3535749b27bd705bd73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edec90d4d716e0e0c715",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "7519531d853ae0ad01b6",
+    "url": "/static/js/2.58211eca.chunk.js"
+  },
+  {
+    "revision": "83e895f9a437e69d1a80f15ce6e50621",
+    "url": "/static/js/2.58211eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edec90d4d716e0e0c715",
+    "url": "/static/js/main.f27f2d98.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "6a5cd70e7c3eb18e32df7b2e940f3e48",
+    "url": "/static/media/logo.6a5cd70e.png"
+  }
+]);
